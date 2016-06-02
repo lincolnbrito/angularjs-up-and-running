ef7c29bbd2d228c992ee1731aeaf84e8dc12ad97
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-06-02 17:04:55 -0300

    fix

--- chapter5/simple-angularjs-service/app.js
+++ chapter5/simple-angularjs-service/app.js
@@ -8,11 +8,32 @@ angular.module("notesApp")
             self.tab = tab;
         }
     }])
-    .controller("SubCtrl",[function(){
+    .controller("SubCtrl",["ItemService", function(ItemService){
         var self = this;
-        self.tab = "first";
 
-        self.open = function(tab){
-            self.tab = tab;
-        }
+        self.list = function() {
+            return ItemService.list();
+        };
+
+        self.add = function(){
+            ItemService.add({
+                id: self.list().length + 1,
+                label: "Item " + self.list().length
+            });
+        };
+    }])
+    .factory("ItemService", [function(){
+        var items = [
+            {id: 1, label: "Item 0"},
+            {id: 2, label: "Item 1"}
+        ];
+
+        return {
+            list: function(){
+                return items;
+            },
+            add: function(item){
+                items.push(item);
+            }
+        };
     }]);
