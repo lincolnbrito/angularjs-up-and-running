2d82ce4abad8786e68d394085a7725fab5f90ecd
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-06-02 16:53:31 -0300

    ssubctrl

--- chapter5/simple-angularjs-service/app.js
+++ chapter5/simple-angularjs-service/app.js
@@ -1,7 +1,18 @@
 angular.module("notesApp", []);
-angular.module("notesApp").controller("MainCtrl",["$log", function($log){
-    var self = this;
-    self.logStuff = function(){
-        $log.log("The button was pressed");
-    }
-}]);
+angular.module("notesApp")
+    .controller("MainCtrl",[function(){
+        var self = this;
+        self.tab = "first";
+
+        self.open = function(tab){
+            self.tab = tab;
+        }
+    }])
+    .controller("SubCtrl",[function(){
+        var self = this;
+        self.tab = "first";
+
+        self.open = function(tab){
+            self.tab = tab;
+        }
+    }]);
