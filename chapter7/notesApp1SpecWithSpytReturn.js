cbf261c340b965d219b942cf54dfdad4d4c20a18
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-06-05 23:35:28 -0300

    spyOn return

--- chapter7/notesApp1SpecWithSpytReturn.js
+++ chapter7/notesApp1SpecWithSpytReturn.js
@@ -1,20 +1,17 @@
-describe("ItemCtrl with spies", function(){
+describe("ItemCtrl with SpyReturn", function(){
     beforeEach(module("notesApp1"));
 
     var ctrl, itemService;
 
     beforeEach(inject(function($controller, ItemService){
-        spyOn(ItemService, "list").and.callThrough();
+        spyOn(ItemService, "list").and.returnValue([{id:1, label:"Mock"}]);
         itemService = ItemService;
         ctrl = $controller("ItemCtrl");
     }));
 
     it("should load mocked out items", function(){
         expect(itemService.list).toHaveBeenCalled();
-        expect(itemService.list.calls.count()).toEqual(1);
-        expect(ctrl.items).toEqual([
-            {id: 1, label: "Item 0"},
-            {id: 2, label: "Item 1"}
-        ]);
+        expect(itemService.list).toHaveBeenCalledTimes(1);
+        expect(ctrl.items).toEqual([{id:1, label:"Mock"}]);
     });
 });
